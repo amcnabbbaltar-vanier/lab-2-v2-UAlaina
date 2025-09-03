a13e83b8feda636aa17cbbb71ce5fecab9587ee2
UAlaina <urmicaa@gmail.com>
2025-09-03 17:38:38 -0400

    Lab 2 Finished.

---
+++ Assets/Scripts/TrapController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapController : MonoBehaviour
+{
+    public float rotationSpeed = 100f;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        Quaternion deltaRotation = Quaternion.Euler(0, rotationSpeed * Time.fixedDeltaTime, 0);
+        rb.MoveRotation(rb.rotation * deltaRotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+    }
+}
